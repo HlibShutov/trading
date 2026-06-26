58b9aeedc15794bd2f2ac298e15741b27a00e0b2
HlibShutov <u140_hlishu_waw@technischools.com>
2026-06-26 20:32:54 +0200

    order service proceeds event

--- order-service/src/main/java/com/trading/order_service/consumer/TradeEventConsumer.java
+++ order-service/src/main/java/com/trading/order_service/consumer/TradeEventConsumer.java
@@ -1,6 +1,7 @@
 package com.trading.order_service.consumer;
 
 import com.trading.order_service.model.TradeExecutedEvent;
+import com.trading.order_service.service.OrderService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.ObjectMapper;
@@ -9,10 +10,15 @@ import tools.jackson.databind.ObjectMapper;
 public class TradeEventConsumer {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    public TradeEventConsumer(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    private final OrderService orderService;
+
     @KafkaListener(topics = "trade-executed", groupId = "order-service")
     public void listen(String payload) {
-        System.out.println("got event");
         TradeExecutedEvent trade = objectMapper.readValue(payload, TradeExecutedEvent.class);
-        System.out.println(trade);
+        orderService.processTrade(trade);
     }
 }
